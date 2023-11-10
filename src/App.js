4d6a48e83da5e50b32f93a610fece4d27060ff6d
ericbo <eric.bowser@maersk.com>
2023-11-09 18:29:54 -0700

    deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Navbar, NavLink, Tab, Tabs } from 'react-bootstrap'
+import {Accordion, Container, Navbar, NavLink, Tab, Tabs} from 'react-bootstrap'
 import Frameworks from './Components/Frameworks'
 import Tools from './Components/Tools'
 import EducationDetails from './Components/Education'
@@ -11,7 +11,7 @@ import {
 } from './Components/Experience'
 import Aspirations from './Components/Aspirations'
 import head from './images/head.jpg'
-import ericDocx from './files/Eric-Bowser-Resume.docx'
+import ericDocx from './files/Eric-Bowser-Resume-2023.docx'
 
 function App () {
   const [tabbedKey, setTabbedKey] = useState('None')
@@ -46,6 +46,32 @@ function App () {
           </NavLink>
         </div>
       </Navbar>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
       <Tabs
         variant={'tabs'}
         id='resume-tabs'
