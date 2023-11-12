cba3e6578b59c818485b9f08422b0cff20bdcb83
ericbowser <ericryanbowser@gmail.com>
2023-11-12 00:52:14 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import {Accordion, Carousel, Container, Navbar, NavLink, Tab, Tabs} from 'react-bootstrap'
+import {Accordion, Card, Carousel, Container, Navbar, NavLink, Tab, Tabs} from 'react-bootstrap'
 import head2 from './images/head2.jpg'
 import  kick_flip from './images/kick_flip.jpg'
 import ollie from './images/kickflip_2.jpg'
-import flip_3 from './images/ollie.jpg'
+import flip_3 from './images/ollie_2.jpg'
+import flip_2 from './images/kickflip_2.jpg'
+import ollie2 from './images/ollie.jpg'
 import ericDocx from './files/Eric-Bowser-Resume-2023.docx'
 import styled from "styled-components";
+import {WtwInfo, GoldPointInfo, ProEdgeInfo, TekSystemsInfo} from "./Components/Experience"
+import Education from "./Components/Education"
+import Tools from "./Components/Tools"
+import Frameworks from "./Components/Frameworks";
 
 const NavbarContainer = styled(Container)`
   margin-top: 30px;
@@ -15,48 +21,52 @@ const NavbarContainer = styled(Container)`
 `;
 
 function App () {
+  const [tabbedKey, setTabbedKey] = useState('None')
+  useEffect(() => {}, [tabbedKey])
 
   return (
-    <NavbarContainer >
-      <Navbar >
-        <Navbar.Brand>
-          <img src={head2} alt={'Me'} style={{width:'auto', height:'170px'}}/>
-        </Navbar.Brand>
-        <div>
-          <NavLink href={ericDocx} style={{ color: 'white' }} download>
-            Resume
-          </NavLink>
-          <NavLink style={{ color: 'white' }} href={'mailto:ericryanbowser@gmail.com'}>
-            Email Eric
-          </NavLink>
-          <NavLink
-            style={{ color: 'white' }}
-            href={'https://github.com/ericbowser'}
-          >
-            Github
-          </NavLink>
-          <NavLink
-            style={{ color: 'white' }}
-            href='/webresume'
-          >
-            Experience
-          </NavLink>
-          
-        </div>
-      </Navbar>
-      
-    <Carousel style={{height: '300px', width: '900px', padding:'125px'}}>
-      <Carousel.Item>
-        <img src={ollie}  alt={''}/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src={ollie}  alt={''}/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src={flip_3}  alt={''}/>
-      </Carousel.Item>
-        </Carousel>
-      </NavbarContainer>
+		<>
+			<Carousel style={{backgroundColor: 'lightgrey'}}>
+				<Carousel.Item>
+					<img src={kick_flip}  alt={''}/>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src={flip_2}  alt={''}/>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src={flip_3}  alt={''}/>
+				</Carousel.Item>
+			</Carousel>
+	  <Card>
+		  <Card.Img src={head2} style={{width: '5%'}}/>
+	  </Card>
+		<Container>
+			<Tabs
+				variant={'tabs'}
+				id='resume-tabs'
+				activeKey={tabbedKey}
+				onSelect={key => setTabbedKey(key)}
+			>
+				<Tab eventKey='education' title='Education'>
+					<Education />
+				</Tab>
+				<Tab eventKey='experience' title='Experience'>
+					<WtwInfo />
+					<TekSystemsInfo />
+					<GoldPointInfo />
+					<ProEdgeInfo />
+				</Tab>
+				<Tab eventKey='frameworks' title='Frameworks'>
+					<Frameworks />
+				</Tab>
+				<Tab eventKey='tools' title='tools'>
+					<Tools />
+				</Tab>
+			
+			</Tabs>
+		</Container>
+		
+			</>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Aspirations from "./Components/Aspirations";
-import Education from "./Components/Education";
-import Tools from "./Components/Tools";
-import Frameworks from "./Components/Frameworks";
-import WebResume from "./Components/WebResume";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-/*
-  {
-    path: '/frameworks',
-    element: <Frameworks />
-  },
-*/
-  {
-    path: '/webresume',
-    element: <WebResume />
-  },
-/*
-  {
-    path: '/tools',
-    element: <Tools />
-  },
-  {
-    path: '/education',
-    element: <Education />
-  },
-  {
-    path: '/aspirations',
-    element: <Aspirations />
-  }
-*/
-])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router}>
-        <App/>
-    </RouterProvider>
+  <React.StrictMode>
+      <App/>
+  </React.StrictMode>
 );
 
 export default root;
