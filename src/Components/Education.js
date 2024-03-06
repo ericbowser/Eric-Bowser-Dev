a0191cb08b9d94a6d518123d433019b5d103a709
unknown <ericryanbowser@gmail.com>
2024-03-06 15:30:41 -0700

    update to pure

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -1,13 +1,15 @@
 ﻿import React from 'react';
 import {HeaderLine} from "../styles";
 
-const Education = () => (
-    <React.Fragment>
-        <HeaderLine>Education</HeaderLine>
-        <ul>
-            <li>Weber State University: 2011-2013 - BA Computer Science</li>
-            <li>University Of Phoenix: 2010-2011 - Associates of Art</li>
-        </ul>
-    </React.Fragment>
-);
+function Education() {
+    return (
+        <React.Fragment>
+            <HeaderLine>Education</HeaderLine>
+            <ul>
+                <li>Weber State University: 2011-2013 - BA Computer Science</li>
+                <li>University Of Phoenix: 2010-2011 - Associates of Art</li>
+            </ul>
+        </React.Fragment>
+    );
+}
 export default Education;
